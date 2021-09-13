e37bb98ab25c78f5adcd1ab35b96047ac82472c9
bryangomezcarranza <bryangomezcarranza@gmail.com>
2021-09-13 11:14:16 -0700

    added model controller

--- BAMF/BAMF/Controllers/Model Controller/MemberController.swift
+++ BAMF/BAMF/Controllers/Model Controller/MemberController.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+class MemberController {
+    static let members = [
+        Member(name: "Bryan Gomez", bio: "23 year old living in Seattle WA. Borned in Mexico City, lived there for 9 years. Likes to work out and play videos games. Recently changed careers to iOS Developing.", funFact: "Participated in an student exchange program in Japan at age 14."),
+        Member(name: "Hyrum Bateman", bio: "Born in San Antonio TX, newly 19 and excited to make your apps. I have a family of 8 and they're all bitter sweet. Love to hang out with the homies and game. Former track athlete, and my favorite event was pole vault. I think all coups are cool.", funFact: "Both of my pinkies are double jointed. I can play the cello, guitar, piano, and ukulele. I can lift x5 my body weight in a combined 3 lifts (squat, deadlift, and benchpress). Definitely an animal whisperer"),
+        Member(name: "Chandler Madsen", bio: "Born in LA, raised in Washington, resides in Utah. Was a barber for 7 years. Decided to change things up and quit barbering to pursue Software Development. Likes working out, playing video games (I suck at them), and traveling. Dream destination is Iceland.", funFact: "I can play 14 different instruments, and I'm friends with kendrick lamars barber")
+    ]
+}
+
